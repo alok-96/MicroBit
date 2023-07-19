7e78c572d8041c3b973c41ef90f3750cf7892a93
alok96 <dwivedialok799@gmail.com>
2023-07-19 12:17:31 +0530

    updated social links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "../styles/Footer.scss";
 import logo from "../assets/asset3.png";
 import {
-    AiFillCodepenCircle,
-    AiFillFacebook,
-    AiFillInstagram,
-    AiFillGithub,
-    AiFillTwitterCircle
+  AiFillLinkedin,
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillGithub,
+  AiFillTwitterCircle,
 } from "react-icons/ai";
 
 const Footer = () => {
@@ -20,11 +20,41 @@ const Footer = () => {
         <p>Copyright &copy; All Right Reserved.</p>
       </div>
       <div className="socials">
-        <AiFillCodepenCircle />
-        <AiFillFacebook />
-        <AiFillTwitterCircle />
-        <AiFillInstagram />
-        <AiFillGithub />
+        <a
+          href="https://www.linkedin.com/in/alok-dwivedi-4347801b8/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillLinkedin />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100070380284005"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillFacebook />
+        </a>
+        <a
+          href="https://twitter.com/alokdwivedi99"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillTwitterCircle />
+        </a>
+        <a
+          href="https://www.instagram.com/_its_alok_07_/"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillInstagram />
+        </a>
+        <a
+          href="https://github.com/alok-96"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillGithub />
+        </a>
       </div>
     </div>
   );
